56bd21767c2ace3a5f1eece9b9af399d11edb0d1
Ro <elangs321@gmail.com>
2017-08-03 01:12:39 -0400

    Update thelogo.py

--- thelogo.py
+++ thelogo.py
@@ -1,32 +1,8 @@
-import console, time, speech
-def logo():
-	console.set_color()
-	console.set_font('AnonymousPro-Bold', 15.35)
-	print "# -=-=-=-=-=-=- #  # -=-=-=-=-=-=- #"
-	print ""
-	print "     ____.------''''--.___"
-	print "         ____.----'---._/ '--"
-	print "              __.----./ '--"
-	print "                    / '-"
-	print "            __..--------..__"
-	print "         _-'              '----'"
-	print "        ||"
-	print "        ||"
-	print "        '.__   Savage"
-	print "            '--------."
-	print "            Official ''-."
-	print "                        |'."
-	print "                       /  |"
-	print "                    __/__.'"
-	print "                   ' /"
-	print ""
-	print "# -=-=-=-=-=-=- #  # -=-=-=-=-=-=- #"
-	print ""
-logo()
-speech.say("Welcome To APDOS")
-time.sleep(1)
-speech.say("Powered by Sav Sec")
-time.sleep(3)
-console.set_font()
-console.clear()
-# SavSec / Savage Official Logo in ACSII (c) 2016
+print """
+  ____               ____            
+ / ___|  __ ___   __/ ___|  ___  ___ 
+ \___ \ / _` \ \ / /\___ \ / _ \/ __|
+  ___) | (_| |\ V /  ___) |  __/ (__ 
+ |____/ \__,_| \_/  |____/ \___|\___|
+        Private Contracting
+"""
